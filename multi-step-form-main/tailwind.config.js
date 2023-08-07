8d0620dc7eff41f7b9d30ef9fd919c8f0f310221
Vinjamara Pavan Kumar <109328446+pavanvinjamara@users.noreply.github.com>
2023-08-07 19:29:46 +0530

    first commit

---
+++ multi-step-form-main/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('/images/bg-sidebar-mobile.svg')",
+          
+        }
+      }
+    }
+  }
